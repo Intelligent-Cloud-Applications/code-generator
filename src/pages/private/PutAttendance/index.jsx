54f982dcb627bdefefb02b2b22851af5caceb5f8
MarkWolf257 <sambhunathmeher@gmail.com>
2024-09-19 19:56:11 +0530

    Modified qr attendance to put to recent class

--- src/pages/private/PutAttendance/index.jsx
+++ src/pages/private/PutAttendance/index.jsx
@@ -1,4 +1,4 @@
-import {useNavigate, useParams} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {useContext, useEffect, useState} from "react";
 import {API, Auth} from "aws-amplify";
 import InstitutionContext from "../../../Context/InstitutionContext";
@@ -8,21 +8,23 @@ import SubmitRating from "./SubmitRating";
 
 const PutAttendance = () => {
   const { InstitutionId } = useContext(InstitutionContext).institutionData;
-  const { isAuth, userData, util } = useContext(Context);
+  const { userData, util } = useContext(Context);
   const { emailId } = userData;
   const navigate = useNavigate();
-  const { classId } = useParams();
+  // const { classId } = useParams();
 
   const [instructorData, setInstructorData ] = useState({});
 
   useEffect(() => {
     const putAttendance = async () => {
-      if (!userData) return;
+      console.log('USERDATA', userData);
+      if (userData === {}) return;
       util.setLoader(true);
       let response;
       try {
         await Auth.currentAuthenticatedUser();
         try {
+          const { classId } = await API.get('main', `/any/get-current-class/${InstitutionId}`, {});
           response = await API.post('main', `/user/put-attendance/${InstitutionId}`, { body: { classId, emailId } });
           setInstructorData(response);
           toast.success("Attendance marked successfully");
@@ -37,7 +39,7 @@ const PutAttendance = () => {
       }
       catch {
         util.setLoader(false);
-        navigate(`/auth/put-attendance/${classId}`);
+        navigate(`/auth/put-attendance`);
       }
     }
 
