3233c5a34a35385ceb1d06e3edd18846e2ae9b12
Podili Biswajit <biswajitofficial2002@gmail.com>
2024-11-10 00:23:01 +0530

    Refactor ContextProvider and Query components for improved readability and functionality; removed console logs, added phone number field, and ensured all form fields are validated before submission.

--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -201,11 +201,9 @@ const ContextProvider = (props) => {
   const getImagesFromAPI = async (imageUrl) => {
     try {
       const response = await API.get('main', `/user/get-imageUrl/${web.InstitutionId}`);
-      console.log(response);
       setImageUrls(response.GalleryImagesLinks || []);
       if (imageUrl) {
         const filename = imageUrl.split('/').pop();
-        console.log(filename)
         if (response.gallery && response.gallery[filename]) {
           setDescription(response.gallery[filename].description);
           setTitle(response.gallery[filename].title);
@@ -227,7 +225,6 @@ const ContextProvider = (props) => {
       try {
         const response = await API.get('main', `/payment-history/${userData.institution}`);
         const payments = response?.payments || [];
-        console.log(response);
         setRevenue(Array.isArray(payments) ? payments : []); // Ensure revenue is an array
       } catch (error) {
         console.error('Error getting payment history from API: ', error);
@@ -239,8 +236,6 @@ const ContextProvider = (props) => {
       paymentDetailsAdmin();
     }
   }, [userData.institution]); // Dependency on userData.institution
-
-  console.log(revenue);
 
   const updateUserStatus = async (userId, institution) => {
     const currentEpochTime = Math.floor(new Date().getTime() / 1000);
--- src/pages/public/Query/index.jsx
+++ src/pages/public/Query/index.jsx
@@ -1,58 +1,68 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 //import emailJs from '@emailjs/browser'
 //import '../Components/comp/Query.css'
-import Footer from '../../../components/Footer'
-import NavBar from '../../../components/Header'
-import { API } from 'aws-amplify'
+import Footer from "../../../components/Footer";
+import NavBar from "../../../components/Header";
+import { API } from "aws-amplify";
 
-import { useContext } from 'react'
-import Context from '../../../Context/Context'
-import './index.css'
-import {Button2} from "../../../common/Inputs";
-import InstitutionContext from '../../../Context/InstitutionContext'
-import { toast } from 'react-toastify'
+import { useContext } from "react";
+import Context from "../../../Context/Context";
+import "./index.css";
+import { Button2 } from "../../../common/Inputs";
+import InstitutionContext from "../../../Context/InstitutionContext";
+import { toast } from "react-toastify";
 
 export default function Query() {
-  const InstitutionData = useContext(InstitutionContext).institutionData
-  const UtilCtx = useContext(Context).util
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [address, setAddress] = useState('')
-  const [message, setMessage] = useState('')
+  const InstitutionData = useContext(InstitutionContext).institutionData;
+  const UtilCtx = useContext(Context).util;
+  const UserCtx = useContext(Context).userData;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
 
   const onSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    UtilCtx.setLoader(true)
+    UtilCtx.setLoader(true);
+
+    if(!name || !email || !phoneNumber || !message){
+      UtilCtx.setLoader(false);
+      return toast.error("All fields are required");
+    }
 
     try {
-
       const apiName = "main";
-      const path = `/any/create-query/${InstitutionData.institutionId}`;
+      const path = `/any/create-query/${InstitutionData.institutionid}`;
       const myInit = {
         body: {
           fullName: name,
-          companyName: null,
           emailId: email,
-          address: address,
-          projectDetails: message,
+          phoneNumber: phoneNumber,
+          message: message,
+          userType: UserCtx.userType || "member",
+          adminEmail: InstitutionData.Query_EmailId,
+          isPaymentRedirect:
+            window.location.href.split("/")[3] === "query" ? false : true,
         },
       };
 
-      await API.post(apiName, path, myInit);
+      await API.put(apiName, path, myInit);
 
-      UtilCtx.setLoader(false)
-      toast.info("Your message is sent. We'll get back to you soon!")
-      setName('')
-      setEmail('')
-      setAddress('')
-      setMessage('')
+      UtilCtx.setLoader(false);
+      toast.success(
+        "Your message has been sent. We will get back to you as soon as possible."
+      );
+      setName("");
+      setEmail("");
+      setPhoneNumber("");
+      setMessage("");
     } catch (e) {
-      toast.error(e.message)
-      UtilCtx.setLoader(false)
+      toast.error(e.message);
+      UtilCtx.setLoader(false);
     }
-  }
+  };
 
   return (
     <>
@@ -75,17 +85,17 @@ export default function Query() {
             <div
               className={`m-2 p-10 max850:hidden w-[18rem] h-[30rem] flex flex-col gap-[1rem]`}
               style={{
-                backgroundColor: InstitutionData.PrimaryColor
+                backgroundColor: InstitutionData.PrimaryColor,
               }}
             >
               <span
                 className={`font-bold sans-sarif text-[1.6rem] w-full text-white leading-7`}
               >
-                Contact Information{' '}
+                Contact Information{" "}
               </span>
               <div className={`text-[0.9rem] sans-sarif text-white`}>
-                {' '}
-                [] Already a Customer or need help ?{' '}
+                {" "}
+                [] Already a Customer or need help ?{" "}
               </div>
               <div className={`flex text-white text-[0.8rem] flex-col`}>
                 <span
@@ -120,7 +130,7 @@ export default function Query() {
                       className={`rounded-md py-[0.4rem] px-2 max500:w-[100%] bg-[#d9d9d980]`}
                       value={name}
                       onChange={(e) => {
-                        setName(e.target.value)
+                        setName(e.target.value);
                       }}
                     />
                   </li>
@@ -132,19 +142,19 @@ export default function Query() {
                       className={`rounded-md py-[0.4rem] px-2 max500:w-[100%] bg-[#d9d9d980]`}
                       value={email}
                       onChange={(e) => {
-                        setEmail(e.target.value)
+                        setEmail(e.target.value);
                       }}
                     />
                   </li>
                   <li
                     className={`flex gap-4 items-center justify-between w-[23rem] max500:flex-col max500:gap-1 max500:items-start max500:w-[90%] max500:mr-20`}
                   >
-                    <label>Address</label>
+                    <label>Phone Number</label>
                     <input
                       className={`rounded-md py-[0.4rem] px-2 max500:w-[100%] bg-[#d9d9d980]`}
-                      value={address}
+                      value={phoneNumber}
                       onChange={(e) => {
-                        setAddress(e.target.value)
+                        setPhoneNumber(e.target.value);
                       }}
                     />
                   </li>
@@ -156,13 +166,13 @@ export default function Query() {
                       className={`rounded-md py-[0.4rem] px-2 h-[8rem] max500:w-[100%] bg-[#d9d9d980]`}
                       value={message}
                       onChange={(e) => {
-                        setMessage(e.target.value)
+                        setMessage(e.target.value);
                       }}
                     />
                   </li>
                 </ul>
                 <Button2
-                  data={'Submit'}
+                  data={"Submit"}
                   fn={onSubmit}
                   w="8rem"
                   className="max500:mr-14"
@@ -174,5 +184,5 @@ export default function Query() {
       </div>
       <Footer />
     </>
-  )
+  );
 }
