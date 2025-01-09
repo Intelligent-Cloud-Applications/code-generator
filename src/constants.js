c70b5415f811c643c9faa50fd88e9b1178385f38
Avishek <avishekmishra56@gmail.com>
2025-01-09 12:19:10 +0530

    `Refactor institution configuration to use data from data.json`

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,18 @@
-const institutionName = 'happyprancer'; // Change this variable to configure for different institutions
+import data from 'data.json';
+
+const { institutionId } = data; // Extract institutionId
+function splitInstitutionId() {
+    const parts = institutionId.split(/(\d+)/).filter(Boolean); // Split into text and numbers
+    return {
+        institution: parts[0], // The text portion
+        id: parts[1] // The numeric portion
+    };
+}
+
+// Usage
+const result = splitInstitutionId();
+
+const institutionName = result.institution; // Change this variable to configure for different institutions
 
 const institutionData = {
     BETA_DOMAIN: `https://beta.${institutionName}.com`, // Beta environment URL
@@ -11,12 +25,12 @@ const institutionData = {
         // Configuration for the beta environment
         [`beta-${institutionName}`]: {
             s3Bucket: `beta.${institutionName}.com`,   // Name of the S3 bucket where the beta frontend is deployed
-            cloudfrontId: 'E263LXOGXF3H0N'            // CloudFront distribution ID for the beta environment
+            cloudfrontId: data.cloudFrontId            // CloudFront distribution ID for the beta environment
         },
         // Configuration for the production environment
         [institutionName]: {
             s3Bucket: `${institutionName}.com`,        // Name of the S3 bucket where the production frontend is deployed
-            cloudfrontId: 'E470TC368F952'             // CloudFront distribution ID for the production environment
+            cloudfrontId: data.cloudFrontId,             // CloudFront distribution ID for the production environment
         }
     },
     // change these SEO meta tags according to the institutions
