9ff0aaeee9d4a2c8d2a6f21cf8727a4a7294c422
Podili Biswajit <biswajitofficial2002@gmail.com>
2025-03-08 20:12:45 +0530

    Bug fix

--- src/Development/common/Flowbite/customTheme.js
+++ src/Development/common/Flowbite/customTheme.js
@@ -222,7 +222,7 @@ const customTheme = {
     "body": {
       "base": "group/body",
       "cell": {
-        "base": "bg-lighestPrimaryColor bg-opacity-30 px-6 py-4 group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg "
+        "base": "bg-lighestPrimaryColor text-white bg-opacity-30 px-6 py-4 group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg "
       }
     },
     "head": {
