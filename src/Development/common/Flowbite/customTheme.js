6d2dda33a4098e84865dfe74494fa8cd68a51498
Podili Biswajit <81102456+biswajitpodili@users.noreply.github.com>
2025-03-11 19:22:29 +0530

    Flowbite implemented in previous session (#91)
    * Flowbite implemented in previous session
    
    * Bug fix

--- src/Development/common/Flowbite/customTheme.js
+++ src/Development/common/Flowbite/customTheme.js
@@ -213,6 +213,30 @@ const customTheme = {
       },
     },
   },
+  table: {
+    "root": {
+      "base": "w-full text-left text-sm text-gray-500 dark:text-gray-400",
+      "shadow": "absolute left-0 top-0 -z-10 h-full w-full rounded-lg bg-white drop-shadow-md dark:bg-black",
+      "wrapper": "relative"
+    },
+    "body": {
+      "base": "group/body",
+      "cell": {
+        "base": "bg-lighestPrimaryColor text-white bg-opacity-30 px-6 py-4 group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg "
+      }
+    },
+    "head": {
+      "base": "group/head text-xs",
+      "cell": {
+        "base": "bg-primaryColor bg-opacity-90 font-semibold text-[1rem] text-white px-6 py-3 group-first/head:first:rounded-tl-lg group-first/head:last:rounded-tr-lg"
+      }
+    },
+    "row": {
+      "base": "group/row",
+      "hovered": "hover:bg-gray-50 dark:hover:bg-gray-600",
+      "striped": "odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700"
+    }
+  }
 };
 
 export default customTheme;
