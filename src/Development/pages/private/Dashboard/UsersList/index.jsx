367d4d007219b6299ed2953fe91c90aef1b79a00
ipsita014 <samantarayreya@gmail.com>
2025-03-12 23:34:39 +0530

    member list updated

--- src/Development/pages/private/Dashboard/UsersList/index.jsx
+++ src/Development/pages/private/Dashboard/UsersList/index.jsx
@@ -102,7 +102,7 @@ const UsersList = ({ userCheck, setUserCheck }) => {
 
   const handleDeleteUser = (institution, cognitoId) => {
     setUserToDelete({ institution, cognitoId });
-    setShowDeleteModal(true); // Open the modal
+    setShowDeleteModal(true);
   };
 
   const confirmDelete = async () => {
@@ -115,7 +115,7 @@ const UsersList = ({ userCheck, setUserCheck }) => {
   };
 
   const formatDate = (epochDate) => {
-    if (!epochDate || isNaN(new Date(epochDate))) return "NA"; // Handle null, undefined, or invalid dates
+    if (!epochDate || isNaN(new Date(epochDate))) return "NA";
 
     const date = new Date(epochDate);
     const day = String(date.getDate()).padStart(2, "0");
@@ -151,24 +151,8 @@ const UsersList = ({ userCheck, setUserCheck }) => {
     return initials;
   };
 
-  const getColor = (name) => {
-    if (!name) return "#888888";
-    const colors = [
-      "#FF5733",
-      "#33FF57",
-      "#5733FF",
-      "#FF5733",
-      "#33FF57",
-      "#5733FF",
-      "#FF5733",
-      "#33FF57",
-      "#5733FF",
-      "#FF5733",
-      "#33FF57",
-      "#5733FF",
-    ];
-    const index = name.length % colors.length;
-    return colors[index];
+  const getColor = () => {
+    return InstitutionData.PrimaryColor;
   };
 
   const mobileProps = {
@@ -402,8 +386,9 @@ const UsersList = ({ userCheck, setUserCheck }) => {
                             >
                               {user.balance}
                             </Table.Cell>
-                            <Table.Cell className="flex gap-3 text-gray-700 font-semibold text-center">
+                            <Table.Cell className="flex items-center gap-3 text-gray-700 font-semibold text-center px-4 py-4">
                               <button
+                                className="p-2 hover:bg-transparent rounded-md"
                                 onClick={() => {
                                   setIsUserAdd(false);
                                   setIsModalOpen(true);
@@ -418,27 +403,26 @@ const UsersList = ({ userCheck, setUserCheck }) => {
                                 <FaEye size={20} />
                               </button>
                               <button
-                                onClick={() =>
-                                  handleDeleteUser(
-                                    user.institution,
-                                    user.cognitoId
-                                  )
-                                }
+                                className="p-2 hover:bg-transparent rounded-md"
+                                onClick={() => handleDeleteUser(user.institution, user.cognitoId)}
                               >
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
                                   viewBox="0 0 24 24"
-                                  fill="currentColor"
-                                  className="w-5"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="size-6"
                                 >
                                   <path
-                                    fillRule="evenodd"
-                                    d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Z"
-                                    clipRule="evenodd"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                                   />
                                 </svg>
                               </button>
                             </Table.Cell>
+
                           </Table.Row>
                         );
                       }
